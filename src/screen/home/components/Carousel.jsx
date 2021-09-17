5c91e02b6b00b88f52ecdc684c783a3940b6318f
DarkTangent01 <mayankksrivastavaa@gmail.com>
2021-09-17 22:02:24 +0530

    fourth updated commit

--- src/screen/home/components/Carousel.jsx
+++ src/screen/home/components/Carousel.jsx
@@ -5,31 +5,46 @@ import image1 from '../../../images/carousel1.png';
 import image2 from '../../../images/carousel2.png';
 import image3 from '../../../images/carouselimage.png';
 import image4 from '../../../images/mobiles.jpg';
+import { makeStyles } from '@material-ui/core/styles';
 
-const contentStyle = {
-  height: '24rem',
-  // color: '#fff',
-  // lineHeight: '23rem',
-  textAlign: 'center',
-  background: '#364d79',
-  marginBottom: '-0.5rem',
-};
+
+const useStyles = makeStyles(theme => ({
+  h3: {
+    height: '24rem',
+    color: '#fff',
+    lineHeight: '23rem',
+    textAlign: 'center',
+    background: '#364d79',
+    marginBottom: '-0.5rem',
+  },
+  img: {
+    width: '1472px',
+    height: '377px',
+    [theme.breakpoints.down('sm')]: {
+      width: '896px',
+    },
+    // [theme.breakpoints.down('sm')]: {
+    //   width: '888px',
+    // },
+  }
+}));
 
 const Carousell = () => {
+  const classes = useStyles();
   return (
     <div>
       <Carousel effect="fade" autoplay >
     <div>
-    <h3 style={contentStyle}><img src={image1} style={{width: "1472px", height: '377px'}} alt="" srcset="" /></h3>
+    <h3 className={classes.h3} ><img src={image1} className={classes.img} alt="images" srcSet="" /></h3>
     </div>
     <div>
-      <h3 style={contentStyle}><img src={image2} style={{width: "1472px", height: '377px'}} alt="" srcset="" /></h3>
+      <h3 className={classes.h3} ><img src={image2} className={classes.img} alt="images" srcSet="" /></h3>
     </div>
     <div>
-      <h3 style={contentStyle}><img src={image3} style={{width: "1472px", height: '377px'}} alt="" srcset="" /></h3>
+      <h3 className={classes.h3} ><img src={image3} className={classes.img} alt="images" srcSet="" /></h3>
     </div>
     <div>
-      <h3 style={contentStyle}><img src={image4} style={{width: "1472px", height: '377px'}} alt="" srcset="" /></h3>
+      <h3 className={classes.h3}><img src={image4} className={classes.img} alt="images" srcSet="" /></h3>
     </div>
   </Carousel>
     </div>
